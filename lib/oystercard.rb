e06793523811c063ae4a371ef663acd1f2746f78
Louise Ballantyne <lou@birdline.co.uk>
2021-03-08 16:13:54 +0000

    6 - used constant to add max balance
    Co-Authored-By: Lou Reade <39278690+loushark@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
 attr_accessor :balance
+DEFAULT_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Balance exceeds limit of £#{DEFAULT_BALANCE}" if (@balance + amount) > DEFAULT_BALANCE
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,12 +13,16 @@ alias_method :card, :subject
     card.top_up(5)
     expect(card.balance).to eq 5
   end
+
+  it "throws an error if balance exceeds limit" do
+    expect { card.top_up(95) }.to raise_error "Balance exceeds limit of £#{Oystercard::DEFAULT_BALANCE}"
+  end
 end
 
 
-#card = Card.new
-#card.add_money(20)
-#card.touch_in(hammersmith)
-#card.touch_out(cockfosters)
-#card.balance
-#station["station name"] returns zone
+# card = Card.new
+# card.add_money(20)
+# card.touch_in(hammersmith)
+# card.touch_out(cockfosters)
+# card.balance
+# station["station name"] returns zone
